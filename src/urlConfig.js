a3ec987c81ec392e1ff8642703ea6cd42ea79863
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-10-10 16:27:42 +0530

    update url config

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,14 @@
-export const api = "http://localhost:2000/api/";
+const baseUrl =
+  process.env.API || "https://d-flipkart-rest-server.herokuapp.com/";
+//const baseUrl = "http://localhost:2000";
+
+export const api = `${baseUrl}/api`;
+
 export const generatePublicUrl = (fileName) => {
-  return `http://localhost:2000/public/${fileName}`;
+  return `${baseUrl}/public/${fileName}`;
 };
+
+// export const api = "http://localhost:2000/api/";
+// export const generatePublicUrl = (fileName) => {
+//   return `http://localhost:2000/public/${fileName}`;
+// };
