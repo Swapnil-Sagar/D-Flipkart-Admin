440a062fcdc17eec9da6dd9120ad1409c88a16e2
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-10-10 16:18:48 +0530

    Admin push

---
+++ src/containers/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import "./style.css";
+
+export default function Home() {
+  return (
+    <Layout sidebar>
+      {/* <Jumbotron
+        style={{ margin: "5rem", background: "#fff" }}
+        className="text-center"
+      >
+        <h1>Welcome to Admin Dashboard</h1>
+      </Jumbotron> */}
+    </Layout>
+  );
+}
